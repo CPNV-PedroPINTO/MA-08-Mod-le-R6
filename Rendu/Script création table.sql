6ba511c92b1c1dd55889ab96097c1cf608aff6a8
CUANY Simon <Simon.CUANY@cpnv.ch>
2021-01-15 11:10:23 +0100

    Add insert data into db with script

--- Rendu/Script création table.sql
+++ Rendu/Script création table.sql
@@ -26,7 +26,7 @@ CREATE TABLE Accessories (
   Type VARCHAR(45) NOT NULL,
   Location VARCHAR(45) NOT NULL,
   Stat VARCHAR(100) NOT NULL,
-  Descript VARCHAR(100) NOT NULL,
+  Descript VARCHAR(45) NOT NULL,
   )
 
   use RainbowSix;
@@ -39,12 +39,13 @@ CREATE TABLE Weapons (
   Recoil VARCHAR(45) NOT NULL,
   ADS VARCHAR(45) NOT NULL,
   Capacity INT NOT NULL,
+  Accessories_id INT,
 )
 
   use RainbowSix;
 drop table if EXISTS Mods
 CREATE TABLE Mods (
-  id INT NOT NULL identity(1,1)  PRIMARY KEY ,
+  id INT NOT NULL   identity(1,1)  PRIMARY KEY ,
   Name VARCHAR(45) NOT NULL UNIQUE,
   Duration INT NOT NULL,
   Special INT NOT NULL,
@@ -56,21 +57,24 @@ CREATE TABLE Stat (
   id INT NOT NULL identity(1,1)  PRIMARY KEY ,
   Defense VARCHAR(45) NOT NULL UNIQUE,
   Speed VARCHAR(45) NOT NULL UNIQUE,
- )
+ )   
   
     use RainbowSix;
 drop table if EXISTS Characters
 CREATE TABLE Characters (
   id INT NOT NULL identity(1,1)  PRIMARY KEY ,
   Name VARCHAR(45) NOT NULL UNIQUE,
-  Part VARCHAR(7) NOT NULL UNIQUE,
-  Ability_name VARCHAR(20) NOT NULL UNIQUE,
-  Ability_definition VARCHAR(200) NOT NULL UNIQUE,
-  Passive_ability VARCHAR(20)  NULL UNIQUE,
-  Passif_definition VARCHAR(200)  NULL UNIQUE,
-  Gender VARCHAR(10) NULL UNIQUE,
-  Country VARCHAR(45) NULL UNIQUE,
-  Special_force VARCHAR(80) NULL UNIQUE,
+  Part VARCHAR(7) NOT NULL ,
+  Ability_name VARCHAR(20) NOT NULL ,
+  Ability_definition VARCHAR(200) NOT NULL ,
+  Passive_ability VARCHAR(20)  NULL ,
+  Passif_definition VARCHAR(200)  NULL ,
+  Gender VARCHAR(10) NULL ,
+  Country VARCHAR(45) NULL ,
+  Special_force VARCHAR(80) NULL ,
+  Weapons_id INT,
+  Rewards_id INT,
+  Gadgets_id INT,
 
  )
 
@@ -78,19 +82,20 @@ CREATE TABLE Characters (
 drop table if EXISTS Maps
 CREATE TABLE Maps (
   id INT NOT NULL identity(1,1)  PRIMARY KEY ,
-  Name VARCHAR(45) NOT NULL UNIQUE,
-  Country VARCHAR(45) NOT NULL UNIQUE,
-  Size INT NOT NULL UNIQUE,
-  Nb_spawns VARCHAR(45) NOT NULL UNIQUE,
-  Nb_floors VARCHAR(45) NOT NULL UNIQUE,
+  Name VARCHAR(45) NOT NULL ,
+  Country VARCHAR(45) NOT NULL ,
+  Size INT NOT NULL ,
+  Nb_spawns VARCHAR(45) NOT NULL ,
+  Nb_floors VARCHAR(45) NOT NULL ,
+  Mods_id INT,
   
  )
      use RainbowSix;
 drop table if EXISTS Rewards
 CREATE TABLE Rewards (
   id INT NOT NULL identity(1,1)  PRIMARY KEY ,
-  Name VARCHAR(45) NOT NULL UNIQUE,
-  Type VARCHAR(45) NOT NULL UNIQUE,
+  Name VARCHAR(45) NOT NULL ,
+  Type VARCHAR(45) NOT NULL ,
    
  )
 
@@ -98,8 +103,22 @@ CREATE TABLE Rewards (
 drop table if EXISTS Gadgets
 CREATE TABLE Gadgets (
   id INT NOT NULL identity(1,1)  PRIMARY KEY ,
-  Name VARCHAR(45) NOT NULL UNIQUE,
-  Definition VARCHAR(200) NOT NULL UNIQUE,
-  Part VARCHAR(200) NOT NULL UNIQUE,
+  Name VARCHAR(45) NOT NULL ,
+  Definition VARCHAR(200) NOT NULL ,
+  Part VARCHAR(200) NOT NULL ,
    
  )
+ ALTER TABLE Weapons WITH CHECK ADD  CONSTRAINT FK_Weapons_Accessories FOREIGN KEY(Accessories_id)
+REFERENCES Accessories(id)
+
+ ALTER TABLE Characters WITH CHECK ADD  CONSTRAINT FK_Characters_Weapons FOREIGN KEY(Weapons_id)
+REFERENCES Weapons(id)
+
+ ALTER TABLE Characters WITH CHECK ADD  CONSTRAINT FK_Characters_Rewards FOREIGN KEY(Rewards_id)
+REFERENCES Rewards(id)
+
+ ALTER TABLE Characters WITH CHECK ADD  CONSTRAINT FK_Characters_Gadgets FOREIGN KEY(Gadgets_id)
+REFERENCES Gadgets(id)
+
+ ALTER TABLE Maps WITH CHECK ADD  CONSTRAINT FK_Maps_Mods FOREIGN KEY(Mods_id)
+REFERENCES Mods(id)
